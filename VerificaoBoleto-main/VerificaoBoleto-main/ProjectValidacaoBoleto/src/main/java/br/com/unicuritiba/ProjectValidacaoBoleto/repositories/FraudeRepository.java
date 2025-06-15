4d6a61e1e356cd6cd38fb6da33d4e532636d7e5b
João Gabriel Carvalho Boese <129881841+JoaoGBoese15@users.noreply.github.com>
2025-06-15 17:03:44 -0300

    Add files via upload

---
+++ VerificaoBoleto-main/VerificaoBoleto-main/ProjectValidacaoBoleto/src/main/java/br/com/unicuritiba/ProjectValidacaoBoleto/repositories/FraudeRepository.java
@@ -0,0 +1,11 @@
+package br.com.unicuritiba.ProjectValidacaoBoleto.repositories;
+
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import br.com.unicuritiba.ProjectValidacaoBoleto.models.Fraude;
+
+public interface FraudeRepository extends JpaRepository<Fraude, Long> {
+    Optional<Fraude> findByCpf(String cpf);
+    Optional<Fraude> findByCodigoBarras(String codigoBarras);
+    Optional<Fraude> findByNome(String nome);
+}
